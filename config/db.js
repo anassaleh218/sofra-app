760392001fc2de2aebcf4c96ee0d0121b304761a
Anas Saleh <99517428+anassaleh218@users.noreply.github.com>
2024-08-06 02:23:34 +0300

    initializing 4

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
 
-const sequelize = new Sequelize({
+const options = {
     host: "sql8.freemysqlhosting.net", // Updated host to match provided details
     dialect: "mysql", // MySQL dialect
     username: "sql8723879", // Updated username
     password: "qewKZaLIjh", // Added password
     database: "sql8723879", // Updated database name
     port: 3306, // Added port number
-    logging: console.log // Logging option
-});
+    logging: console.log, // Logging option
+    dialectModule: mysql2 // Use mysql2 as the dialect module
+};
+
+const sequelize = new Sequelize(options);
 
 module.exports = sequelize;
